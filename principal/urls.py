ed1d7beb8c73e13d40b2b056fcaecae52da039f6
Aritz Blesa <aritzblesa@gmail.com>
2025-08-16 11:14:01 -0300

    comenzando segunda pagina

--- principal/urls.py
+++ principal/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # Esta ruta es la que se usará para la página de inicio
+    path('plan/', views.segunda_pagina, name='segunda_pagina'), # Esta ruta es para la segunda página
 ]
