66139e0d3729024db15ca75a45a57af64c2bec01
Aritz Blesa <aritzblesa@gmail.com>
2025-08-16 11:14:01 -0300

    comenzando segunda pagina

--- principal/views.py
+++ principal/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'principal/index.html')
+def segunda_pagina(request):
+    return render(request, 'principal/segunda_pagina.html')
