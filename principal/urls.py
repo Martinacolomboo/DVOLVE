7ddcd8b92ad267e8052bf59f0548ac2de2e55d93
Aritz Blesa <aritzblesa@gmail.com>
2025-08-23 01:18:39 -0300

    cambios visuales

--- principal/urls.py
+++ principal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # Esta ruta es la que se usará para la página de inicio
+    path('', views.home, name="home"), # Esta ruta es la que se usará para la página de inicio
     path('plan/', views.segunda_pagina, name='segunda_pagina'), # Esta ruta es para la segunda página
 ]
