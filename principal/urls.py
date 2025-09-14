b2d8bc9632b6baa63e0a29d08bc7ccb9265be4fb
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-09-14 00:37:21 -0300

    Cambios en la segunda_pagina, agrego footer y politicas de privacidad

--- principal/urls.py
+++ principal/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"), # Esta ruta es la que se usará para la página de inicio
     path('plan/', views.segunda_pagina, name='segunda_pagina'), # Esta ruta es para la segunda página
+    path('pagos/', views.pagos, name='pagos'),
+    path("privacidad/", views.privacidad, name="privacidad")
+
 ]
--- principal/views.py
+++ principal/views.py
@@ -6,3 +6,7 @@ def home(request):
     return render(request, 'principal/home.html')
 def segunda_pagina(request):
     return render(request, 'principal/segunda_pagina.html')
+def pagos(request):
+    return render(request, 'principal/pagos.html')
+def privacidad(request):
+    return render(request, "principal/privacidad.html")
