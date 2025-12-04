03ee8ea677f4465aef48942ced0db3a671e1f2fa
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-12-04 19:34:41 -0300

    arreglamos cosas parte 1

--- webLula/settings/production.py
+++ webLula/settings/production.py
@@ -2,32 +2,15 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    ".fly.dev",
-    "tu-dominio.com",
-]
-
+ALLOWED_HOSTS = ["*"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "5432"),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
+     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-# Cloudflare R2
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-AWS_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("R2_BUCKET")
-AWS_S3_ENDPOINT_URL = os.getenv("R2_URL")
-AWS_S3_REGION_NAME = "auto"
-AWS_S3_SIGNATURE_VERSION = "s3v4"
