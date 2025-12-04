a507c36273d4bf873f0135735658b345ff08cdae
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-12-04 20:55:59 -0300

    cambios necesarios

--- webLula/settings/base.py
+++ webLula/settings/base.py
@@ -90,6 +90,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/clientes/"
