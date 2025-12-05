9ec856b24519ecbaa6817fac3efc971c4a7a7b0d
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-12-05 02:05:58 -0300

    agrego admin parte 4

--- webLula/wsgi.py
+++ webLula/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from frontend.utils.create_superuser import create_superuser_if_not_exists
 
-# crea admin automáticamente si no existe
-create_superuser_if_not_exists()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webLula.settings.production')
 
 application = get_wsgi_application()
