f950b4362dfb7d1d74b8820e2703c14be7fcf627
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-12-05 10:11:10 -0300

    agrego admin parte 5

--- create_superuser.py
+++ create_superuser.py
@@ -4,18 +4,18 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webLula.settings")
 django.setup()
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 username = os.getenv("ADMIN_USER")
-email = os.getenv("ADMIN_EMAIL")
 password = os.getenv("ADMIN_PASS")
 
 if not username or not password:
-    print("Faltan variables de entorno ADMIN_USER / ADMIN_PASS")
+    print("❌ Faltan credenciales en variables de entorno (ADMIN_USER / ADMIN_PASS)")
     exit()
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuario creado con éxito!")
+    User.objects.create_superuser(username=username, password=password)
+    print(f"✔ Superusuario '{username}' creado en producción.")
 else:
-    print("El superusuario ya existe.")
+    print(f"ℹ El usuario '{username}' ya existe.")
