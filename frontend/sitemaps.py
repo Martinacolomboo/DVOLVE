cafe095d183208e3b46e3d41a4dac7257e72fbf4
Martinacolomboo <martinanataliacolombo@gmail.com>
2025-12-15 13:02:18 -0300

    Google tratando que funcione parte 7

--- frontend/sitemaps.py
+++ frontend/sitemaps.py
@@ -1,12 +1,19 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
+
 class StaticViewSitemap(Sitemap):
-    priority = 0.5
-    changefreq = 'weekly'
+    priority = 0.8
+    changefreq = "weekly"
 
     def items(self):
-        return ['principal:home']  # Nombres de tus vistas
+        return [
+            "principal:principal",      # home
+            "principal:home",
+            "principal:segunda_pagina",
+            "principal:pagos",
+            "principal:privacidad",
+        ]
 
     def location(self, item):
         return reverse(item)
